c58123d426f37913114aab3e6ec58f4ee2fccaf7
Rahulalvk <rahulal432@gmail.com>
2023-05-03 21:46:51 -0700

    Prime number between the given limit of number

---
+++ PrimeNumber/PrimeNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber
+{
+    internal class PrimeNumber
+    {
+        public static void DisplayPrimeNumber()
+        {
+            int i = 1;
+            Console.WriteLine("Enter the starting value");
+            int Min = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the ending value");
+            int Max = Convert.ToInt32(Console.ReadLine());
+
+            for (int n = Min; n <= Max; n++)
+            {
+                while (n % i != 0)
+                {
+                    i++;
+                }
+                Console.WriteLine(n);
+
+
+            }
+        
+        }
+    }
+}
